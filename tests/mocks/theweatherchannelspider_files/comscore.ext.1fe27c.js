9ddbe59b0477d77992e6855b4046428e7a1a2e40
robocode2 <32843931+robocode2@users.noreply.github.com>
2025-02-09 15:46:23 +0100

    Delete tests/mocks directory

--- tests/mocks/theweatherchannelspider_files/comscore.ext.1fe27c.js
+++
@@ -1,3 +0,0 @@
-window._comscore=window._comscore||[],window._comscore.push({c1:"2",c2:"9576127"}),function(){var e=document.createElement("script"),c=document.getElementsByTagName("script")[0];e.async=!0,e.src=(document.location.protocol=="https:"?"https://sb":"http://b")+".scorecardresearch.com/beacon.js",c.parentNode.insertBefore(e,c)}();
-
-//# sourceMappingURL=comscore.ext.1fe27c.js.map
--- tests/mocks/theweatherchannelspider_files/polyfill.min.js
+++
@@ -1,5 +0,0 @@
-/*
- * Polyfill service v4.8.0
- * Disable minification (remove `.min` from URL path) for more info
-*/
-
